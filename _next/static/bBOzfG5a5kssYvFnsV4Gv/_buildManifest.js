bc151176274c789788d91d13b6f58278cb62cb93
rychlostwebu <115408482+rychlostwebu@users.noreply.github.com>
2022-12-28 22:14:10 +0100

    Add files via upload

---
+++ _next/static/bBOzfG5a5kssYvFnsV4Gv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c5c3ea6ba6c3cfc3.css","static/chunks/16-d61711be4962ece6.js","static/chunks/pages/index-c5aad8a3b25c7034.js"],"/_error":["static/chunks/pages/_error-b56f3b256830f921.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
